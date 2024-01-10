33daffbe6bb0372ea70a5167cfbdea36fdd388e1
Laura <lau.webster@hotmail.com>
2024-01-10 15:41:34 -0300

    validações

--- gestao_vagas/src/main/java/br/com/laurawebster/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/laurawebster/gestao_vagas/modules/candidate/controllers/CandidateEntity.java
@@ -1,6 +1,10 @@
 package br.com.laurawebster.gestao_vagas.modules.candidate.controllers;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
 import java.util.UUID;
 
 @Data
@@ -8,8 +12,12 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaços")
     private String username;
+    @Email(message = "O campo [e-mail] deve conter um e-mail válido")
     private String email;
+    @Length(min = 10, max = 100)
     private String password;
     private String description;
     private String curriculum;
